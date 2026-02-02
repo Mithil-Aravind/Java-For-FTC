087f1353203256ee92b2f5d3109feec9ed7c271a
Mithil-Aravind <jokestermithil@gmail.com>
2026-02-01 18:51:29 -0800

    Implement basic teleop control for driving left

---
+++ TeleOpAttempt.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class TeleOpAttempt extends LinearOpMode {
+  if (gampad1.b) {
+    driveLeft.setPower(1);
+  }
+}
